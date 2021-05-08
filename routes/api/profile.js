2e54da632dc470b5e740bbc5064c85625db533c1
Emily Sheng <emily.s2442@gmail.com>
2021-05-08 09:21:24 -0700

    Completed user route code

---
+++ routes/api/profile.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+
+
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+        'user', 
+        ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+
+        res.json(profile);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   POST api/profile
+// @desc    Create or update a user profile
+// @access  Private
+router.post('/', [auth, [
+    check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+    check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false}))
+
+// API endpoint
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
